ffe15690a08c7c48357486c5ab4157f19a820303
Daniel Avila <messagedaniel@protonmail.com>
2023-04-15 09:21:59 -0700

    feat: add .env and /node_modules to .gitignore feat: add README.md feat: add package.json with dependencies and devDependencies feat: add server/index.js with database connection, middleware, routes, and server start feat: add sum.test.js with a test for adding 1 + 2 to equal 3

---
+++ server/index.js
@@ -0,0 +1,50 @@
+(async () => {
+  // Import required modules
+  const express = require('express');
+  const { Pool } = require('pg');
+  const bcrypt = require('bcrypt');
+  const session = require('express-session');
+  require('dotenv').config();
+
+  // Initialize the app
+  const app = express();
+
+  // Database connection
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+  // Middleware
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+  }));
+
+  // Routes
+  app.get('/', (req, res) => {
+    res.send('Welcome to the Express app with PostgreSQL and user authentication!');
+  });
+
+  // Login route
+  app.post('/login', async (req, res) => {
+    // ... (same as before)
+  });
+
+  // Logout route
+  app.post('/logout', (req, res) => {
+    // ... (same as before)
+  });
+
+  // Start the server
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+})();
