05a73edf11564b49cb1e86cc74c93973ad25e130
Danny Avila <messagedaniel@protonmail.com>
2023-06-01 21:55:25 -0400

    refactor(index.js): remove unused session middleware feat(index.js): add loaderioPath variable to serve static files from public directory

--- server/index.js
+++ server/index.js
@@ -1,21 +1,15 @@
 const client = require('./redis');
 
 (async () => {
+  const path = require('path');
   require('dotenv').config();
   const express = require('express');
-  const session = require('express-session');
   const routes = require('./routes');
-
   const app = express();
-  app.use(express.static(path.join(__dirname, 'public')));
+  const loaderioPath = path.join(__dirname, 'public');
+  console.log(loaderioPath)
+  app.use(express.static(loaderioPath));
   app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
-  }));
 
   app.use('/products', routes);
   const port = process.env.PORT || 3000;
