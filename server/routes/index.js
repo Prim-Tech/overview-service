ee56978ace1566297abfa6708f49eb9e0430fab6
Danny Avila <messagedaniel@protonmail.com>
2023-06-02 14:37:28 -0400

    refactor(routes/index.js): rename route '/2' to '/new' to better reflect its purpose

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const controllers = require('../controllers');
 
 router.get('/', controllers.product.getAll);
-router.get('/2', controllers.product.getAll2);
+router.get('/new', controllers.product.getAll2);
 router.get('/:product_id', controllers.product.getOne);
 router.get('/:product_id/styles', controllers.product.getStyles2);
 router.get('/:product_id/related', controllers.product.getRelated);
