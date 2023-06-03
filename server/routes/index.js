44250ede63f0020b6185dcbf0865c7cfefdb5286
Danny Avila <messagedaniel@protonmail.com>
2023-06-03 11:43:47 -0400

    refactor(index.js): reorder routes to match the order of the controllers used

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const controllers = require('../controllers');
 const controllersWithoutRedis = require('../controllers/noRedis');
 
-router.get('/', controllers.product.getAll);
-router.get('/:product_id', controllers.product.getOne);
-router.get('/:product_id/styles', controllers.product.getStyles);
-router.get('/:product_id/related', controllers.product.getRelated);
 router.get('/noredis/', controllersWithoutRedis.product.getAll);
 router.get('/noredis/:product_id', controllersWithoutRedis.product.getOne);
 router.get('/noredis/:product_id/styles', controllersWithoutRedis.product.getStyles);
 router.get('/noredis/:product_id/related', controllersWithoutRedis.product.getRelated);
+
+router.get('/', controllers.product.getAll);
+router.get('/:product_id', controllers.product.getOne);
+router.get('/:product_id/styles', controllers.product.getStyles);
+router.get('/:product_id/related', controllers.product.getRelated);
 
 module.exports = router;
